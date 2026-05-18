3e36c0920d48a65500175fb295bc0a41927d724c
frpbypass12208 <makaiphanvtiw@hotmail.com>
2026-05-19 00:49:13 +0200

    Fix babel-plugin-module-resolver compilation error

--- babel.config.js
+++ babel.config.js
@@ -2,15 +2,5 @@ module.exports = function (api) {
   api.cache(false)
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      [
-        'module-resolver',
-        {
-          alias: {
-            'expo-web-browser': './modules/expo-web-browser',
-          },
-        },
-      ],
-    ],
   }
 }
