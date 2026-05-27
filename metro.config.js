1632a3bb88cc165cb2eea883e7b92d432f081c7d
frpbypass12208 <makaiphanvtiw@hotmail.com>
2026-05-27 23:11:30 +0200

    Fix Babel compilation error by restoring metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,26 +1,5 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
 
 const config = getDefaultConfig(__dirname);
-
-const stubModules = {
-  'expo-auth-session': path.resolve(__dirname, 'modules/expo-auth-session.ts'),
-  'expo-web-browser': path.resolve(__dirname, 'modules/expo-web-browser.ts'),
-};
-
-const originalResolveRequest = config.resolver.resolveRequest;
-
-config.resolver.resolveRequest = (context, moduleName, platform) => {
-  if (stubModules[moduleName]) {
-    return {
-      filePath: stubModules[moduleName],
-      type: 'sourceFile',
-    };
-  }
-  if (originalResolveRequest) {
-    return originalResolveRequest(context, moduleName, platform);
-  }
-  return context.resolveRequest(context, moduleName, platform);
-};
 
 module.exports = config;
