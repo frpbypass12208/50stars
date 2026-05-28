7839064a7016a85090cb30e5470ccfe5888ff284
frpbypass12208 <makaiphanvtiw@hotmail.com>
2026-05-28 11:34:12 +0200

    Fix Babel @babel/traverse compilation error

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,16 @@
 module.exports = function (api) {
-  api.cache(true)
+  api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-  }
-}
+    presets: [
+      [
+        'babel-preset-expo',
+        {
+          // Disable auto-inclusion of reanimated plugin to avoid
+          // @babel/traverse version conflict (FunctionParent in undefined)
+          reanimated: false,
+        },
+      ],
+    ],
+    plugins: ['react-native-reanimated/plugin'],
+  };
+};
